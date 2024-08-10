42e4d28ee549c5032923bf1a16fd664357a5a0b1
NeolGG <neolgutierrez@gmail.com>
2024-08-10 03:19:29 -0400

    added fetch, parse functions

---
+++ class_search.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+
+import requests
+import json
+from urllib.parse import quote
+
+
+# Define the URL for the request
+url = "https://classsearch.nd.edu/api/?page=fose&route=search&camp=M&stat=A%2CF"
+
+def fetch_data(query: str):
+    api_query = quote(query)
+    headers = {'Content-Type': 'application/json',  'Accept': 'application/json',}
+    payload = _create_payload(api_query)
+    
+    response = requests.post(url, headers=headers, data=payload)
+
+    if response.status_code != 200:
+        raise requests.exceptions.HTTPError(f"HTTPError: {response.status_code} - {response.reason}")
+        
+    data = response.json()
+    
+    return data
+
+def _create_payload(keyword:str):
+    '''
+    creates encoded payload for post request
+    payload includes data for:\n
+    - fall 2024 ("srcdb": "202410")\n
+    - main campus("field": "camp","value": "M")\n
+    '''
+    payload = {
+        "other": {"srcdb": "202410"},
+        "criteria": [
+            {"field": "camp","value": "M"},
+            {"field": "keyword","value": f"{keyword}"},
+            {"field": "stat","value": "A,F"}
+        ]
+    }
+    
+    encoded_payload = json.dumps(payload)
+
+    return encoded_payload
+
+def parse_course_data(json_data: list) -> list:
+    courses = list()
+    
+    if json_data['count'] == 0:
+        print("No user data found")
+        return courses
+
+    for r in json_data['results']:
+        listing = (f"{r["code"]} {r["title"]} - {r["instr"]}")
+        if listing not in courses:
+            courses.append(listing)
+
+    return courses
+
+if __name__ == "__main__":
+    query = "joanna"
+    
+    data = fetch_data(query)
+    
+    courses = parse_course_data(data)
+    
+    print(courses)
+    
+
