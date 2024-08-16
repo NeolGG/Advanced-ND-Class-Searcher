37116ae90ad7b2e7fa73abd98492700e8fb73ec4
NeolGG <neolgutierrez@gmail.com>
2024-08-16 04:49:42 -0400

    WIP novo functions, will be used for find a room functionality

--- novo_search.py
+++ novo_search.py
@@ -7,8 +7,11 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import requests
 import json
+import os
+import time
 
 url = "https://bxeregprod.oit.nd.edu/StudentRegistration/ssb/searchResults/searchResults"
+json_folder = "jsons/novo"
 
 def _get_cookies() -> dict:
     '''
@@ -55,32 +58,60 @@ def _create_session() ->requests.Session:
     
     return session
 
-def _create_params():
+def _create_params(page_offset:int = 0, max_page: int = 100):
     params = {
         "txt_campus": "M",
-        "chk_include_0": "false", # sunday
-        "chk_include_1": "true",  # monday
-        "chk_include_2": "false", # tuesday
-        "chk_include_3": "false",  # wednesday
-        "chk_include_4": "false", # thursday
-        "chk_include_5": "false", # friday
-        "chk_include_6": "false", # saturday
+        # "chk_include_0": "false", # sunday
+        # "chk_include_1": "false",  # monday
+        # "chk_include_2": "false", # tuesday
+        # "chk_include_3": "false",  # wednesday
+        # "chk_include_4": "false", # thursday
+        # "chk_include_5": "false", # friday
+        # "chk_include_6": "false", # saturday
         "txt_term": "202410", # fall
         "startDatepicker": "",
         "endDatepicker": "",
         "uniqueSessionId": "", # IGNORE
-        "pageOffset": "0",
-        "pageMaxSize": "500",
+        "pageOffset": f"{page_offset}",
+        "pageMaxSize": f"{max_page}",
         "sortColumn": "subjectDescription",
         "sortDirection": "asc"
     }
     
     return params
 
-session = _create_session()
-params = _create_params()
-response = session.get(url,params=params)
+def get_all_classes():
+    if not os.path.exists(json_folder):
+        os.makedirs(json_folder)
+        
+    t = time.time()
+    session = _create_session()
+    data = []
+    
+    for i in range(0,16):
+        params = _create_params(i*500,500)
+        print(f"getting page {i+1}")
+        response = session.get(url,params=params)
+        
+        data.append(response.json())
 
-data = response.json()
+    with open(f"{json_folder}/all_classes{t}.json", 'w') as file:
+        json.dump(data, file, indent=4)
 
-print(json.dumps(data, indent=4))
+def set_filters():
+    buildings = ['"O\'Shaughnessy Hall"', '"Geddes Hall"', '"Haggar Hall"', '"Decio Faculty Hall"',
+     '"Stinson Remick Hall"', '"Biolchini Hall (LAW)"', '"Remick Family Hall"', '"Fitzpatrick Hall of Eng."',
+     '"Grace Hall"', '"Hammes Mowbray Hall"', '"Washington Hall"', '"Walsh Hall of Architecture"',
+     '"Coleman Morse Center"', '"McCourtney Hall"', '"Hayes Healy Center"', '"Innovation Park"',
+     '"Eck Hall of Law"', '"Galvin Life Science Center"', '"DeBartolo Performing Arts Ctr."', '"Crowley Hall"',
+     '"Riley Hall"', '"Corbett Family Hall"', '"Mendoza College of Business"', '"Cushing Hall of Engineering"',
+     '"Bond Hall"', '"Malloy Hall"', '"Ricci Band Building"', '"Jenkins and Nanovic Hall"', '"Jordan Hall of Science"',
+     '"Hesburgh Library"', '"DeBartolo Hall"', '"West Lake Hall"', '"Pasquerilla Center"', '"West Lake Design Studio"',
+     '"Nieuwland Science Hall"', '"Flanner Hall"', '"O\'Neill Hall of Music"', '"Stayer Center"', '"Main Building"']
+    
+    
+
+
+
+
+
